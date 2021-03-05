fe27ebe40355ebdf07f2ae23f2b0c541a4a2933a
Luiz Matos <luiz.matos@amedigital.com>
2021-03-05 19:56:22 -0300

    chore

--- posts_gatherer/db/dynamo.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def get_last_searched_date():
-    return os.getenv('START_DATE')
-
-
-def save_last_searched_date(last_searched_date):
-    return None
--- posts_gatherer/db/mongo.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-import os
-from pymongo import MongoClient
-
-client = MongoClient(os.getenv("MONGODB_URL"))
-mongo_db = client.reddit_posts_gatherer
