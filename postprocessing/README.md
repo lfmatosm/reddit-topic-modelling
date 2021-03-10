a783e10b952158e8c30c57cef2e638527cad7532
Luiz Matos <luiz.matos@amedigital.com>
2021-03-10 00:38:09 -0300

    feat

---
+++ postprocessing/README.md
@@ -0,0 +1,7 @@
+# postprocessing
+
+python postprocessing/postprocess.py --csvs /home/luizmatos/Projetos/UFF/Python/reddit-topic-modelling/postprocessing/postprocessing_test/csvs/lda_results.csv --embeddings /home/luizmatos/Projetos/UFF/Python/reddit-topic-modelling/embeddings/ptwiki_20180420_300d_optimized.w2v --lemma_word_mapping /home/luizmatos/Projetos/UFF/Python/reddit-topic-modelling/datasets/processed/TEST_lemmatized_nouns_only/lemmatized_nouns_only_en[word_lemma_maps].json
+
+# test
+
+python postprocessing/postprocess_test.py --csvs /home/luizmatos/Projetos/UFF/Python/reddit-topic-modelling/postprocessing/postprocessing_test/csvs/lda_results.csv
---
+++ postprocessing/postprocess_test.py
@@ -0,0 +1,27 @@
+from gensim.models import KeyedVectors
+import numpy as np
+import pandas as pd
+import argparse
+import os
+import joblib
+
+parser = argparse.ArgumentParser(description='Postprocessing test')
+parser.add_argument('--csvs', nargs='+', help='list CSV files', required=True)
+args = parser.parse_args()
+
+
+for csv in args.csvs:
+    print(f'Postprocessing models found in CSV: {csv}...')
+    training_results = pd.read_csv(csv)
+    models_paths = training_results['path'].tolist()
+    models_base_path = os.path.sep.join(csv.split(os.path.sep)[:-2])
+
+    for model_path in models_paths:
+        path_to_load = os.path.join(models_base_path, "models", model_path)
+        print(f'Loading model at: {path_to_load}')
+        model = joblib.load(path_to_load)
+        for idx, topic in enumerate(model["topics"]):
+            print(f'Topic: {",".join(topic)} - most similar words: {model["most_similar_words"][idx][:5]}\n')
+        del model
+
+print(f'Postprocessing test finished')
