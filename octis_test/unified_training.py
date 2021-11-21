7ab2de329f442e8de40c077f17901cb826aaabea
Luiz Matos <luiz.matos@amedigital.com>
2021-11-21 18:00:11 -0300

    feat: adds alpha/beta hyperparam dimensions to lda optimization

--- octis_test/unified_training.py
+++ octis_test/unified_training.py
@@ -7,7 +7,7 @@ from octis.models.ETM import ETM
 from octis.evaluation_metrics.diversity_metrics import TopicDiversity
 from octis.evaluation_metrics.coherence_metrics import Coherence
 from octis.optimization.optimizer import Optimizer
-from skopt.space.space import Real, Integer
+from skopt.space.space import Categorical, Integer
 import argparse
 import os
 
@@ -17,6 +17,8 @@ parser.add_argument('--embeddings_path', type=str, default='empty', help='path f
 parser.add_argument('--models', type=str, nargs='+', default=[], required=False)
 parser.add_argument('--all-models', dest='all_models', action='store_true')
 parser.set_defaults(all_models=False)
+parser.add_argument('--min_k', type=int, default=3, help='Minimum no. of topics (k) in the search space', required=False)
+parser.add_argument('--max_k', type=int, default=30, help='Maximum no. of topics (k) in the search space', required=False)
 args = parser.parse_args()
 
 models_to_train = ["lda", "ctm", "etm"] if args.all_models else args.models
@@ -29,28 +31,45 @@ dataset.load_custom_dataset_from_folder(args.dataset_path)
 
 language = args.dataset_path.split(os.path.sep)[-1]
 
+topics_dimension = Integer(low=int(args.min_k), high=int(args.max_k))
+
 models = []
 for model_name in models_to_train:
     if model_name == "lda":
-        models.append(LDA())
+        models.append({
+            "model": LDA(),
+            "search_space": {
+                "num_topics": topics_dimension,
+                "alpha": Categorical(["asymmetric", "auto"]),
+                "eta": Categorical([None, "auto"])
+            }
+        })
     elif model_name == "ctm":
-        models.append(
-            CTM(inference_type="combined", bert_model="distiluse-base-multilingual-cased-v1" if language == "pt" else "bert-base-nli-mean-tokens"),
-        )
+        bert_model = "distiluse-base-multilingual-cased-v1" \
+            if language == "pt" else "bert-base-nli-mean-tokens"
+        models.append({
+            "model": CTM(inference_type="combined", 
+                bert_model=bert_model),
+            "search_space": {
+                "num_topics": topics_dimension,
+            }
+        })
     elif model_name == "etm":
-        models.append(
-            ETM(train_embeddings=False, embeddings_type='keyedvectors', embeddings_path=args.embeddings_path),
-        )
+        models.append({
+            "model": ETM(train_embeddings=False, embeddings_type='keyedvectors', 
+                embeddings_path=args.embeddings_path),
+            "search_space": {
+                "num_topics": topics_dimension,
+            }
+        })
+
 
 coherence_metric = Coherence(topk=10, measure="c_npmi", texts=dataset.get_corpus())
 
-# Define the search space. To see which hyperparameters to optimize, see the topic model's initialization signature
-search_space = {"num_topics": Integer(low=3, high=30)}
-
 for i in range(len(models)):
-    # Initialize an optimizer object and start the optimization.
     optimizer=Optimizer()
-    result = optimizer.optimize(models[i], dataset, coherence_metric, search_space, save_path="../optm_results", # path to store the results
+    result = optimizer.optimize(models["model"][i], dataset, coherence_metric, models["search_space"][i], 
+                                save_path="../optm_results", # path to store the results
                                 number_of_call=30, # number of optimization iterations
                                 model_runs=5, plot_best_seen=True) # number of runs of the topic model
     #save the results of the optimization in file
